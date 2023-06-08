9e15a0f24ae3a547e436c4158cf31de7aa39a132
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-06-08 19:45:23 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿
 
 
-
+/* напишите цикл , который принимакт на вход 2 числа A B и возводит A в натуральную степень  B
 int Prompt(string message)
 {
-    System.Console.Write(message);
+    
     string readInput = System.Console.ReadLine();
     int result = int.Parse(readInput);
     return result;
@@ -33,3 +33,26 @@ if (ValideteExponent(exponent))
 {
     System.Console.WriteLine($"число{powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
 }
+
+Напишите программу которая принимает на вход число и выдает сумму чисел в числе.
+*/
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number 10 %;
+        number = number / 10;
+    }
+    return result;
+}
+int number = Prompt("Ввведите число: ");
+System.Console.Write($"сумма всех чисел в цифре {number}) = {SumAllDigit(number)}");
