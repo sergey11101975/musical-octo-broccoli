df882ed2f281569f63d0c4f1fc32855e8106ae11
Sergey11101975 <bogoslovskiisv@yandex.ru>
2023-06-08 18:10:11 +0300

    1 задача

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValideteExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValideteExponent(exponent))
+{
+    System.Console.WriteLine($"число{powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
